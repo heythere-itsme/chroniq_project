0302cf3ca96574f178277f1e8eed997f6ae0f380
Mayank_Kumar <mayankkumar.current@gmail.com>
2025-07-30 10:58:58 +0530

    Side, TimerRow

--- src/components/DashboardPage/RightSection.tsx
+++ src/components/DashboardPage/RightSection.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import SidebarCalendar from "../Sidebar/SidebarCalendar";
+import TimerRow from "../Sidebar/TimerRow";
 
 const RightSection = () => {
   return (
-    <div className="w-100">
+    <div className="w-100 flex flex-col justify-between">
       <h2 className="font-medium pt-2 px-2">
         {new Date().toLocaleString("en-GB", { month: "short", day: "2-digit" })}
         , {new Date().getFullYear()}
       </h2>
       <SidebarCalendar />
+      <TimerRow />
     </div>
   );
 };
